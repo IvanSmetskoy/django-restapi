f00b671124c2c838d94dfa98bd0c3ccacfb69e47
user name <user email>
2022-10-29 20:50:30 +0600

    modify gitignore and remove secrets

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8888/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"}) # HTTP GET request
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!!!"}) # HTTP GET request
 #print(get_response.text)
 #print(get_response.status_code)
 
